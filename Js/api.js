1b3148c7a8e383a259a11b7110813525d62bfe3f
Daniel Román <dordone46758@universidadean.edu.co>
2022-06-10 00:35:40 -0500

    Actualización 10/06/2022 12:34am COL

--- Js/api.js
+++ Js/api.js
@@ -1,12 +1,17 @@
-function iniciarMap(){
-    var coord = {lat: 4.6626538,lng: -74.0560612};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 4.6626538, lng: -74.0560612 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
 
+window.initMap = initMap;
